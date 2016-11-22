8dd853fc2341441d4158635ae373f18cb17c1804
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-21 20:31:52 -0600

    Initial commit

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ variable "aws_key_path" {}
 
 variable "aws_key_name" {}
 
-# Use the private key in the AWS console to decrypt the password after downloading the .rdp file
-#resource "aws_key_pair" "keys" {
-#  key_name   = "${var.aws_key_name}"
-#  public_key = "${file("${var.aws_key_path}")}"
-#}
-
 resource "aws_instance" "win-vm" {
   count         = 1
   ami           = "ami-1e4c8476"        # Microsoft Windows Server 2012 R2 Base - ami-1e4c8476
