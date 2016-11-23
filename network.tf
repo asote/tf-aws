44c61107f4edf469a36b02b36076d4bb38646076
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-23 00:08:24 -0600

    Modified VPC and SG

--- network.tf
+++ network.tf
@@ -1,14 +1,15 @@
-resource "aws_vpc" "as-vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "dedicated"
+resource "aws_vpc" "default" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "dedicated"
+  enable_dns_hostnames = true
 
   tags {
-    Name = "asotelo-vpc"
+    Name = "terraform-aws-vpc"
   }
 }
 
 resource "aws_subnet" "tier1-sub" {
-  vpc_id            = "${aws_vpc.as-vpc.id}"
+  vpc_id            = "${aws_vpc.default.id}"
   availability_zone = "us-east-1b"
   cidr_block        = "10.0.1.0/24"
 
@@ -18,7 +19,7 @@ resource "aws_subnet" "tier1-sub" {
 }
 
 resource "aws_subnet" "tier2-sub" {
-  vpc_id            = "${aws_vpc.as-vpc.id}"
+  vpc_id            = "${aws_vpc.default.id}"
   availability_zone = "us-east-1b"
   cidr_block        = "10.0.2.0/24"
 
@@ -28,7 +29,7 @@ resource "aws_subnet" "tier2-sub" {
 }
 
 resource "aws_subnet" "tier3-sub" {
-  vpc_id            = "${aws_vpc.as-vpc.id}"
+  vpc_id            = "${aws_vpc.default.id}"
   availability_zone = "us-east-1b"
   cidr_block        = "10.0.3.0/24"
 
--- securitygrp.tf
+++ securitygrp.tf
@@ -1,16 +1,24 @@
-resource "aws_security_group" "tier1-sg" {
-  name        = "tier1-sg"
-  description = "Security group for subnet aws_subnet.tier1"
-  vpc_id      = "${aws_vpc.as-vpc.id}"
-
-  tags {
-    Name = "security-win-vm"
-  }
+resource "aws_security_group" "web" {
+  name        = "vpc_web"
+  description = "Allow incoming HTTP connections."
 
   ingress {
     from_port   = 3389
     to_port     = 3389
-    protocol    = "TCP"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "WebServerSG"
   }
 }
