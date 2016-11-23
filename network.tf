3d3d45a0fd92ba2e34a5b9316fb66f1aff47c11f
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-22 19:14:43 -0600

    Fixed subnets

--- network.tf
+++ network.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
     }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "tier1" {
     vpc_id = "${aws_vpc.main.id}"
     availability_zone = "us-east-1b"
     cidr_block = "10.0.1.0/24"
@@ -18,7 +18,7 @@ resource "aws_subnet" "main" {
 }
 
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "tier2" {
     vpc_id = "${aws_vpc.main.id}"
     availability_zone = "us-east-1b"
     cidr_block = "10.0.2.0/24"
@@ -28,7 +28,7 @@ resource "aws_subnet" "main" {
     }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "tier3" {
     vpc_id = "${aws_vpc.main.id}"
     availability_zone = "us-east-1b"
     cidr_block = "10.0.3.0/24"
