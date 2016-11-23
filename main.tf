e5fb7be7645cb898ed496853035718f9559202ab
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-23 07:33:59 -0600

    changed networing

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 variable "aws_key_name" {}
 
 resource "aws_instance" "win-vm" {
-  count                       = 1
-  ami                         = "ami-83d2f894"                   # Microsoft Windows Server 2012 R2 Base - ami-83d2f894
-  instance_type               = "t2.micro"
-  availability_zone           = "us-east-1b"
-  vpc_security_group_ids      = ["${aws_security_group.web.id}"]
+  count                  = 1
+  ami                    = "ami-83d2f894"                   # Microsoft Windows Server 2012 R2 Base - ami-83d2f894
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1b"
+  vpc_security_group_ids = ["${aws_security_group.web.id}"]
+
   subnet_id                   = "${aws_subnet.tier1-sub.id}"
   associate_public_ip_address = true
   source_dest_check           = false
