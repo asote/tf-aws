b16ac54f741efb29002354c5ddfe9c5aba0c526e
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-22 19:05:33 -0600

    Initial commit

---
+++ network.tf
@@ -0,0 +1,39 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "dedicated"
+
+    tags {
+        Name = "main"
+    }
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = "${aws_vpc.main.id}"
+    availability_zone = "us-east-1b"
+    cidr_block = "10.0.1.0/24"
+
+    tags {
+        Name = "tier1"
+    }
+}
+
+
+resource "aws_subnet" "main" {
+    vpc_id = "${aws_vpc.main.id}"
+    availability_zone = "us-east-1b"
+    cidr_block = "10.0.2.0/24"
+
+    tags {
+        Name = "tier2"
+    }
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = "${aws_vpc.main.id}"
+    availability_zone = "us-east-1b"
+    cidr_block = "10.0.3.0/24"
+
+    tags {
+        Name = "tier3"
+    }
+}
