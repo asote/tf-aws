2ee53a38658eb36ff6121fef96cd06100f1b0758
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-22 19:29:13 -0600

    added AZ

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "win-vm" {
   count                  = 1
   ami                    = "ami-83d2f894"                     # Microsoft Windows Server 2012 R2 Base - ami-83d2f894
   instance_type          = "t2.micro"
+  availability_zone      = "us-east-1b"
   vpc_security_group_ids = ["${aws_security_group.tier1.id}"]
   subnet_id              = "${aws_subnet.tier1.id}"
   key_name               = "${var.aws_key_name}"
