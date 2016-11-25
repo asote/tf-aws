01d74ead9388e7015b4355bbcf02e4303d85c27b
Antonio Sotelo <ASotelo6@gmail.com>
2016-11-25 00:21:38 -0600

    addded provisioner

--- instances-tier1.tf
+++ instances-tier1.tf
@@ -8,10 +8,11 @@ resource "aws_instance" "web" {
   source_dest_check = false
   key_name          = "${var.aws_key_name}"
 
+  # Enable WinRM
   connection {
     type     = "winrm"
     user     = "Administrator"
-    password = "${var.admin_password}"
+    password = "${var.admin_password}" # This is also the password to log in.
   }
 
   user_data = <<EOF
@@ -33,4 +34,31 @@ EOF
     Application   = ""
     Environment   = ""
   }
+}
+
+# Install IIS
+resource "null_resource" "copy_scripts" {
+  provisioner "file" {
+    source      = "conf/Install-IIS.ps1"
+    destination = "C:/Install-IIS.ps1"
+  }
+
+  depends_on = ["aws_instance.web"]
+}
+
+resource "null_resource" "install" {
+  provisioner "remote-exec" {
+    inline = [
+      "powershell.exe -sta -ExecutionPolicy Unrestricted -file C:\\Install--IIS.ps1",
+    ]
+  }
+
+  connection {
+    type     = "winrm"
+    user     = "Administrator"
+    password = "${var.admin_password}" # This is also the password to log in.
+    timeout  = "10m"
+  }
+
+  depends_on = ["null_resource.copy_scripts"]
 }
