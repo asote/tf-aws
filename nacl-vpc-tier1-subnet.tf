decdc3d95a326aef9a2bc41d7447308e35974374
Antonio Sotelo <ASotelo6@gmail.com>
2017-02-06 20:23:47 -0600

    Use of env variables and modified nacl

--- nacl-vpc-tier1-subnet.tf
+++ nacl-vpc-tier1-subnet.tf
@@ -12,7 +12,7 @@ resource "aws_network_acl" "tier1-sub" {
   }
 }
 
-resource "aws_network_acl_rule" "http" {
+resource "aws_network_acl_rule" "http-in" {
   network_acl_id = "${aws_network_acl.tier1-sub.id}"
   rule_number    = 100
   egress         = false
@@ -23,9 +23,9 @@ resource "aws_network_acl_rule" "http" {
   to_port        = 80
 }
 
-resource "aws_network_acl_rule" "https" {
+resource "aws_network_acl_rule" "https-in" {
   network_acl_id = "${aws_network_acl.tier1-sub.id}"
-  rule_number    = 110
+  rule_number    = 200
   egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
@@ -34,9 +34,9 @@ resource "aws_network_acl_rule" "https" {
   to_port        = 443
 }
 
-resource "aws_network_acl_rule" "rdp" {
+resource "aws_network_acl_rule" "rdp-in" {
   network_acl_id = "${aws_network_acl.tier1-sub.id}"
-  rule_number    = 120
+  rule_number    = 300
   egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
@@ -45,13 +45,57 @@ resource "aws_network_acl_rule" "rdp" {
   to_port        = 3389
 }
 
-resource "aws_network_acl_rule" "outbound" {
+resource "aws_network_acl_rule" "custom_tcp-in" {
   network_acl_id = "${aws_network_acl.tier1-sub.id}"
-  rule_number    = 130
-  egress         = true
-  protocol       = "-1"
+  rule_number    = 400
+  egress         = false
+  protocol       = "tcp"
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
-  from_port      = "0"
-  to_port        = "0"
+  from_port      = 1024
+  to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "http-out" {
+  network_acl_id = "${aws_network_acl.tier1-sub.id}"
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
+resource "aws_network_acl_rule" "https-out" {
+  network_acl_id = "${aws_network_acl.tier1-sub.id}"
+  rule_number    = 200
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
+resource "aws_network_acl_rule" "rdp-out" {
+  network_acl_id = "${aws_network_acl.tier1-sub.id}"
+  rule_number    = 300
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "10.0.0.128/25"
+  from_port      = 3389
+  to_port        = 3389
+}
+
+resource "aws_network_acl_rule" "custom_tcp-out" {
+  network_acl_id = "${aws_network_acl.tier1-sub.id}"
+  rule_number    = 400
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
 }
